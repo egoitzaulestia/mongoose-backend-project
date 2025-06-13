e84bd1f30e0d9fb08b0d048222d34e1435d825a1
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-13 22:13:31 +0200

    app connection to mongodb atlas done

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const { dbConnection } = require("./config/config");
+
+app.use(express.json());
+
+dbConnection();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
