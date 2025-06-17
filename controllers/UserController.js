bb2f94e9466eba273dccfe478886d9515ceb7846
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-17 16:28:41 +0200

    UserController register method created

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,2 +1,17 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
+const { jwt_secret } = require("../config/keys");
+
+const UserContreller = {
+  async register(req, res) {
+    try {
+      const user = await User.create({ ...req.body, role: "user" });
+      res.status(201).send({ message: "User registered successfully", user });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .send({ message: "Error while trying to regist a user", error });
+    }
+  },
+};
