52367c2732461cba6f1b6538dae790581d7516d7
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-17 23:19:20 +0200

    user schema model upadted to avoid retriving tokens and password

--- models/User.js
+++ models/User.js
@@ -13,6 +13,14 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// We avoid returning tokens array and password
+UserSchema.methods.toJSON = function () {
+  const user = this._doc;
+  delete user.tokens;
+  delete user.password;
+  return user;
+};
+
 const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
