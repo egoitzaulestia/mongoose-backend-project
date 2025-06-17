92f2da5ee1eedd89c57dde198fd507390bd46802
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-17 18:20:33 +0200

    user login route added

--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,6 @@ const UserController = require("../controllers/UserController");
 
 router.post("/register", UserController.register);
 router.get("/confirm/:emailToken", UserController.confirm);
+router.post("/login", UserController.login);
 
 module.exports = router;
