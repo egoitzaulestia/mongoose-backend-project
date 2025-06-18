6459a374217f34e952336a3de6038a38a093ede5
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-18 22:26:53 +0200

    post schema model ready

--- models/Post.js
+++ models/Post.js
@@ -1,15 +1,64 @@
 const mongoose = require("mongoose");
+const ObjectId = mongoose.SchemaTypes.ObjectId;
+const { isURL } = require("validator");
 
-const PostSchema = new mongoose.Schema(
+// Subdocument schema with timestamps for each like
+const LikeSchema = new mongoose.Schema(
   {
-    title: String,
-    content: String,
+    userId: {
+      type: ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
+
   { timestamps: true }
 );
 
+// Post schema
+const PostSchema = new mongoose.Schema(
+  {
+    author: {
+      type: ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    title: {
+      type: String,
+      trim: true,
+      maxlength: [100, "Title too long"],
+    },
+
+    content: {
+      type: String,
+      required: true,
+      minlength: 1,
+      trim: true,
+    },
+
+    imageUrl: {
+      type: String,
+      trim: true,
+      validate: {
+        validator: (v) => !v || (isURL(v) && /\.(jpe?g|png|gif)$/i.test(v)),
+        message: "Must be a valid image URL",
+      },
+    },
+
+    likes: {
+      type: [LikeSchema], // Like subdocument schema
+      default: [],
+    },
+  },
+
+  { timestamps: true }
+);
+
+// Full-text search index on both 'title' and 'content'
 PostSchema.index({
   title: "text",
+  content: "text",
 });
 
 const Post = mongoose.model("Post", PostSchema);
