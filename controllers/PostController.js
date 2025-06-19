541cf9341e2be45f5c670ba7c31824dbef92b557
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-19 16:15:55 +0200

    created post method ready

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -3,8 +3,18 @@ const Post = require("../models/Post");
 const PostController = {
   async create(req, res) {
     try {
+      const post = Post.create(req.body);
+
+      res.status(201).send({
+        message: "Post created successfully",
+        post,
+      });
     } catch (error) {
       console.error(error);
+      res.status(500).send({
+        message: "Proble while creating a post",
+        error,
+      });
     }
   },
 };
