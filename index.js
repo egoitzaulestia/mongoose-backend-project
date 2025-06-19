8eaa32094da6a7419887964a293e7a9e53ccc7a1
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-19 15:53:02 +0200

    posts endpoint added to index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const { typeError } = require("./middlewares/typeError");
 app.use(express.json());
 
 app.use("/users", require("./routes/users"));
+app.use("/posts", require("./routes/posts"));
 
 dbConnection();
 
