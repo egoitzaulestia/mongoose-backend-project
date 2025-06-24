b9e2620c6c8e782170cb86994c24fef3a8a1d422
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-25 01:30:59 +0200

    unlike comment route ready

--- src/routes/comments.js
+++ src/routes/comments.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const CommentController = require("../controllers/CommentController");
+const { authentication } = require("../middlewares/authentication");
 
 router.get("/", CommentController.getAllComments);
 
 // Like a comment
-router.post("/:commentId/like", CommentController.likeComment);
+router.post("/:commentId/like", authentication, CommentController.likeComment);
 
 // Unlike a comment
+router.delete(
+  "/:commentId/like",
+  authentication,
+  CommentController.unlikeComment
+);
 
 module.exports = router;
