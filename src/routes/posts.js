4868e4463c5dee01121d52276cd0bda000ec5fd4
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-24 22:02:12 +0200

    get all post with comments and authors router ready

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -6,6 +6,7 @@ const { authentication } = require("../middlewares/authentication");
 
 router.post("/create", authentication, PostController.create);
 router.get("/", PostController.getAll);
+router.get("/full", authentication, PostController.getAllWithCommonts);
 router.put("/id/:_id", authentication, PostController.update);
 router.delete("/id/:_id", authentication, PostController.delete);
 router.get("/title/:title", PostController.getByTitle);
