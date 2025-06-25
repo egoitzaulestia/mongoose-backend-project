f3b461291cfac5703b00e8c6aa75a3b25d09e88b
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-25 17:00:01 +0200

    user register with single upload (multer) ready

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/UserController");
+const { single } = require("../middlewares/uploads");
 
 const { authentication, isAdmin } = require("../middlewares/authentication");
 
-router.post("/register", UserController.register);
+// router.post("/register", UserController.register);
+router.post("/register", single("phot"), UserController.register);
+
 router.get("/confirm/:emailToken", UserController.confirm);
 router.post("/login", UserController.login);
 router.delete("/logout", authentication, UserController.logout);
