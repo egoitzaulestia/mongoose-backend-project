66fbf2e8da75a6f038f9bc656c5c04da6ae5970b
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-27 00:53:42 +0200

    welcome message to the API

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 require("dotenv").config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const { dbConnection } = require("./config/db");
 const { typeError } = require("./middlewares/typeError");
 
