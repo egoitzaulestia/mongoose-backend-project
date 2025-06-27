ef57df69f4ff453c384374c7b0257cabfaafab31
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-27 14:13:42 +0200

    basicinfo update

--- src/doc/basicinfo.js
+++ src/doc/basicinfo.js
@@ -2,7 +2,7 @@ module.exports = {
   openapi: "3.0.3",
   info: {
     version: "1.0.0",
-    title: "mongoose-backend-project",
+    title: "mongoose-backend-project API",
     description: "mongoose-backend-project's Social Media API",
   },
 };
