a63e9ef79224c282c036f8b3e82e03722d4af6b8
egoitzaulestia <egoitzaulestia@gmail.com>
2025-09-08 14:32:17 +0200

    backend welcome message updated

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -3,10 +3,10 @@ const HomeController = {
     try {
       return res.status(200).json({
         message:
-          "🎉 Welcome to the Social Media API! Everything's up and running.",
+          "🎉 Welcome to The Social Network API! Everything's up and running.",
       });
     } catch (err) {
-      console.error("HomeController.welcom:", err);
+      console.error("HomeController.welcome:", err);
       return res.status(500).json({
         message: "Server error while loading Social Media API home endpoint",
       });
