7a0d297bf4c7bc2589e2c48222330868d74815f2
egoitzaulestia <egoitzaulestia@gmail.com>
2025-09-10 15:49:29 +0200

    index updated

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
+
 const PORT = process.env.PORT || 3001;
 const { dbConnection } = require("./config/db");
 const { typeError } = require("./middlewares/typeError");
@@ -10,9 +11,23 @@ const { typeError } = require("./middlewares/typeError");
 const swaggerUI = require("swagger-ui-express");
 const docs = require("./docs/index");
 
-app.use(cors());
+// app.use(cors());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      "https://<your-frontend-domain>", // if you deploy the frontend
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
+
+app.get("/health", (req, res) => res.send("ok"));
 
 app.use("/", require("./routes/home"));
 app.use("/users", require("./routes/users"));
